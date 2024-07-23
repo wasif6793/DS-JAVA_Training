867a52a28f595390d1e5dec68f4f522d638aafa2
Mohammad Wasif <wasif6793@gmail.com>
2024-07-23 09:48:45 +0530

    vote

--- AdditionTwoNumberWithDifferentDataType.java
+++ AdditionTwoNumberWithDifferentDataType.java
@@ -1,3 +1,12 @@
 public class AdditionTwoNumberWithDifferentDataType {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 7.5f;
+        int k = (int) b;
+        int c = a + k;
+
+
+        System.out.println(c);
+    }
 
 }
--- Helloworld.java
+++ Helloworld.java
@@ -1,12 +1,7 @@
 public class Helloworld {
 
     public static void main(String[] args) {
-        int a = 5;
-        float b = 7.5f;
-        int k = (int) b;
-        int c = a +k;
 
-
-        System.out.println(c);
+        System.out.println("Wasif");
     }
 }
