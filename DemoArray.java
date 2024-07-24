cf95da76a67992156b732bd9f5dc931ec7029904
Mohammad Wasif <wasif6793@gmail.com>
2024-07-24 12:09:52 +0530

    Printing name using aray

--- DemoArray.java
+++ DemoArray.java
@@ -1,12 +1,11 @@
 public class DemoArray {
     public static void main(String[] args) {
 
-        String[] name = new String[5];
-        name = new String[]{"Wasif", "Amjad", "Jaish", " Harsh", "Rohit"};
+      String[] name = new String[]{"Wasif", "Amjad", "Jaish", " Harsh", "Rohit"};
 
         for (int i = 0; i < name.length; i++) {
 
-            System.out.print(name[i] + " ");
+            System.out.print(name[i] + ", ");
 
         }
     }
