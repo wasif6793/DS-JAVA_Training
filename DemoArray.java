a66c63676276d6f0e631d0174967f17d95e06384
Mohammad Wasif <wasif6793@gmail.com>
2024-07-24 11:53:00 +0530

    Array Example

---
+++ DemoArray.java
@@ -0,0 +1,13 @@
+public class DemoArray {
+    public static void main(String[] args) {
+
+        String[] name = new String[5];
+        name = new String[]{"Wasif", "Amjad", "Jaish", " Harsh", "Rohit"};
+
+        for (int i = 0; i < name.length; i++) {
+
+            System.out.print(name[i] + " ");
+
+        }
+    }
+}
