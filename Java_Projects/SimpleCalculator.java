550817c09ad1461423f95bd94ab7edd96af05ef1
Mohammad Wasif <wasif6793@gmail.com>
2024-07-29 18:36:41 +0530

    Simple calculator

---
+++ Java_Projects/SimpleCalculator.java
@@ -0,0 +1,65 @@
+package Java_Projects;
+
+import java.awt.geom.RectangularShape;
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        calculator();
+    }
+
+    static void calculator(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input first value: ");
+        int val1 = sc.nextInt();
+
+        System.out.println();
+
+        System.out.print("Input second value: ");
+        int val2 = sc.nextInt();
+
+        System.out.print("Select operator '*, /, +, - :");
+
+        String operator = sc.next();
+
+            double result = 0;
+
+            switch (operator){
+                case "*":
+
+                    result = val1 * val2;
+                    System.out.println("Answer: " + result);
+                    break;
+
+                case "+":
+                    result = val1 + val2;
+                    System.out.println("Answer: " + result);
+                    break;
+
+                case "-":
+                    result = val1 - val2;
+                    System.out.println("Answer: " + result);
+                    break;
+
+                case "/":
+                    if(val2 == 0){
+                        System.out.println("Not define");
+                    } else {
+                        result = (double) val1 / val2;
+                        System.out.println("Answer: " + result);
+                    }
+                    break;
+
+                default:
+                    System.out.println("Not a valid operation.");
+            }
+
+
+
+
+
+    }
+}
