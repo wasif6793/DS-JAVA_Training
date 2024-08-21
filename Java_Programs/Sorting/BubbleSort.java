e20af32ff4302f60bc21a48bd155e33a04ccf8f9
Mohammad Wasif <wasif6793@gmail.com>
2024-08-22 05:13:47 +0530

    Spring Project

---
+++ Java_Programs/Sorting/BubbleSort.java
@@ -0,0 +1,4 @@
+package Java_Programs.Sorting;
+
+public class BubbleSort {
+}
---
+++ Spring_Programs/DemoSpring/src/main/java/org/wasif/Laptop.java
@@ -0,0 +1,10 @@
+package org.wasif;
+
+public class Laptop {
+    public Laptop() {
+        System.out.println("Laptop Constructor");
+    }
+    public void compile(){
+        System.out.printf("Its Laptop");
+    }
+}
